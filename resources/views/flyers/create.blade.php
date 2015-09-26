54dd2c96474430dd67996dcfd83df68a551d469d
pranayaryal <drpranayaryal@gmail.com>
2015-09-26 19:57:13 -0400

    initial commit

---
+++ resources/views/flyers/create.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layout')
+
+
+@section('content')
+    <h1>Selling Your Home?</h1>
+
+    <hr>
+
+    <form method="POST" action="/flyers" enctype="multipart/form-data">
+        @include('flyers.form')
+    </form>
+@stop
+
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Project Flyer</h1>
+        <p>
+            This a jumbotron taken from bootstrap
+        </p>
+
+        <a href="/flyers/create" class="btn btn-primary">Create a Flyer</a>
+
+    </div>
+
+
+@stop
